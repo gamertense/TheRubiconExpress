ca1e503c4f5a19e6b7b4d7d0e2e62b351824f47e
Antonio Jack <gamertense@live.com>
2017-11-18 13:53:00 +0700

    Add category table

--- 1rubicon.sql
+++ 1rubicon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2017 at 12:51 PM
+-- Generation Time: Nov 18, 2017 at 07:52 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -34,6 +34,33 @@ CREATE TABLE `cart` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_id`, `cu_id`, `product_id`, `quantity`) VALUES
+(1, 1, 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(20) NOT NULL,
+  `category_product` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`, `category_product`) VALUES
+(1, 'Electronic devices', ''),
+(2, 'Food', '');
 
 -- --------------------------------------------------------
 
@@ -142,28 +169,29 @@ CREATE TABLE `product` (
   `price` double(10,2) NOT NULL,
   `image` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `comment` int(11) NOT NULL
+  `comment` int(11) NOT NULL,
+  `category_id` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`product_id`, `name`, `price`, `image`, `description`, `comment`) VALUES
-(2, 'Samsung Galaxy S8 ', 22000.00, 'product-images/s8.jpg', 'Samsung Galaxy S8 \r\n-Samsung latest smartphone Flagship, came with top of the line gorgeous 5.8 inches 1440p super AMOLED Screen, 12MP camera and lightning fast CPU, GPU and RAM. \r\n', 0),
-(3, 'iPhone X', 40000.00, 'product-images/ix.jpg', 'This chilled salad of noodles and crisp bell peppers tossed in a sesame oil-rice vinegar dressing provides a quick side dish or light meal. Great for make-ahead lunches too.', 0),
-(4, 'Samsung Galaxy Note 8', 32000.00, 'product-images/note8.jpg', 'Thai Basil Chicken dish has spectacular taste even with regular basil instead of Thai or holy basil. The sauce actually acts like a glaze as the chicken mixture cooks over high heat. The recipe works best if you chop or grind your own chicken and have all ingredients prepped before you start cooking.', 0),
-(5, 'google pixel 2', 33000.00, 'product-images/google.jpg', 'Tom Yum Koong,a famous Thai delicacy,is a spicy soup with shrimp in it. River shrimp sometimes use as shrimp', 0),
-(7, 'HTC One A9', 15000.00, 'product-images/one.jpg', 'Porridge With Fish,a boiled rice with steamed fish this chinese style breakfast is famous for its simplicity and taste.\r\n', 0),
-(8, 'Nokia 5', 9000.00, 'product-images/nikia.jpg', 'Thai Spicy Grilled Pork Salad(Yum Moo Yang)This is a spicy and sour grilled pork salad recipe that is cheap and easy . The cut of pork used is shoulder blade, which is not too fatty, but not too dry – it is just right! ', 0),
-(9, 'Special Fish Cakes', 55.00, 'food-images/Thaifishcakes.jpg', 'A fishcake is a food item similar to a croquette, consisting of filleted fish or other seafood with potato patty, sometimes coated in breadcrumbs or batter, and fried.', 0),
-(10, 'Casseroled Shrimps With Glass Noodles', 50.00, 'food-images/maxresdefault (1).jpg', '', 0),
-(11, 'Pork Stir-Fried With Garlic And Peppercorns', 40.00, 'food-images/thai-fried-pork-with-garlic-and-pepper-recipe.jpg', '', 0),
-(12, 'Fried Chicken', 35.00, 'food-images/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg', '', 0),
-(13, 'Green Chicken Curry', 40.00, 'food-images/global-recipes-01_cropped-green_curry2.jpg', '', 0),
-(15, 'Crispy Wonton', 30.00, 'food-images/crispy-pork-wontons.jpg', '', 0),
-(17, 'Honey Rost Duck', 70.00, 'food-images/HoneyRoastedDuck.jpg', '', 0),
-(18, 'Quick-Fried Water Spinach Seasoned With Chili And Soy Sauce', 45.00, 'food-images/maxresdefault (2).jpg', '', 0);
+INSERT INTO `product` (`product_id`, `name`, `price`, `image`, `description`, `comment`, `category_id`) VALUES
+(2, 'Samsung Galaxy S8 ', 22000.00, 'product-images/s8.jpg', 'Samsung Galaxy S8 \r\n-Samsung latest smartphone Flagship, came with top of the line gorgeous 5.8 inches 1440p super AMOLED Screen, 12MP camera and lightning fast CPU, GPU and RAM. \r\n', 0, 1),
+(3, 'iPhone X', 40000.00, 'product-images/ix.jpg', 'This chilled salad of noodles and crisp bell peppers tossed in a sesame oil-rice vinegar dressing provides a quick side dish or light meal. Great for make-ahead lunches too.', 0, 1),
+(4, 'Samsung Galaxy Note 8', 32000.00, 'product-images/note8.jpg', 'Thai Basil Chicken dish has spectacular taste even with regular basil instead of Thai or holy basil. The sauce actually acts like a glaze as the chicken mixture cooks over high heat. The recipe works best if you chop or grind your own chicken and have all ingredients prepped before you start cooking.', 0, 1),
+(5, 'google pixel 2', 33000.00, 'product-images/google.jpg', 'Tom Yum Koong,a famous Thai delicacy,is a spicy soup with shrimp in it. River shrimp sometimes use as shrimp', 0, 1),
+(7, 'HTC One A9', 15000.00, 'product-images/one.jpg', 'Porridge With Fish,a boiled rice with steamed fish this chinese style breakfast is famous for its simplicity and taste.\r\n', 0, 1),
+(8, 'Nokia 5', 9000.00, 'product-images/nikia.jpg', 'Thai Spicy Grilled Pork Salad(Yum Moo Yang)This is a spicy and sour grilled pork salad recipe that is cheap and easy . The cut of pork used is shoulder blade, which is not too fatty, but not too dry – it is just right! ', 0, 1),
+(9, 'Special Fish Cakes', 55.00, 'food-images/Thaifishcakes.jpg', 'A fishcake is a food item similar to a croquette, consisting of filleted fish or other seafood with potato patty, sometimes coated in breadcrumbs or batter, and fried.', 0, 2),
+(10, 'Casseroled Shrimps With Glass Noodles', 50.00, 'food-images/maxresdefault (1).jpg', '', 0, 2),
+(11, 'Pork Stir-Fried With Garlic And Peppercorns', 40.00, 'food-images/thai-fried-pork-with-garlic-and-pepper-recipe.jpg', '', 0, 2),
+(12, 'Fried Chicken', 35.00, 'food-images/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg', '', 0, 2),
+(13, 'Green Chicken Curry', 40.00, 'food-images/global-recipes-01_cropped-green_curry2.jpg', '', 0, 2),
+(15, 'Crispy Wonton', 30.00, 'food-images/crispy-pork-wontons.jpg', '', 0, 2),
+(17, 'Honey Rost Duck', 70.00, 'food-images/HoneyRoastedDuck.jpg', '', 0, 2),
+(18, 'Quick-Fried Water Spinach Seasoned With Chili And Soy Sauce', 45.00, 'food-images/maxresdefault (2).jpg', '', 0, 2);
 
 -- --------------------------------------------------------
 
@@ -188,6 +216,12 @@ ALTER TABLE `cart`
   ADD PRIMARY KEY (`cart_id`),
   ADD KEY `cu_id` (`cu_id`),
   ADD KEY `product_id` (`product_id`) USING BTREE;
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `comment`
@@ -232,7 +266,13 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `comment`
