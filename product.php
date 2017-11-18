593024492d09fc74f5a03b37e8c52ae496a12ab1
Antonio Jack <gamertense@live.com>
2017-11-18 10:25:39 +0700

    Change action-button colors and add footer

--- product.php
+++ product.php
@@ -40,19 +40,19 @@ if (isset($_POST['Register'])) {
                     <article class="col-item">
                         <div class="photo">
                             <div class="options-cart-round">
-                                <button name="addButton" class="btn btn-default" title="Add to cart"
+                                <button name="addButton" class="btn btn-success" title="Add to cart"
                                         data-toggle="tooltip" value="<?php echo $row["product_id"]; ?>">
                                     <span class="fa fa-shopping-cart"></span>
                                 </button>
                             </div>
                             <div class="options-wishlist-round">
-                                <button name="wishButton" class="btn btn-default" title="Add to wishlist"
+                                <button name="wishButton" class="btn btn-danger" title="Add to wishlist"
                                         data-toggle="tooltip" value="<?php echo $row["product_id"]; ?>">
                                     <span class="fa fa-heart"></span>
                                 </button>
                             </div>
                             <div class="options-info-round">
-                                <button name="infoButton" class="btn btn-default" title="More info"
+                                <button name="infoButton" class="btn btn-primary" title="More info"
                                         data-toggle="tooltip" value="<?php echo $row["product_id"]; ?>">
                                     <span class="fa fa-search"></span>
                                 </button>
@@ -78,6 +78,99 @@ if (isset($_POST['Register'])) {
         ?>
     </form>
 </div>
+<footer>
+    <div class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> Lorem Ipsum </h3>
+                    <ul>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> Lorem Ipsum </h3>
+                    <ul>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> Lorem Ipsum </h3>
+                    <ul>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> Lorem Ipsum </h3>
+                    <ul>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                        <li><a href="#"> Lorem Ipsum </a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
+                    <h3> Lorem Ipsum </h3>
+                    <ul>
+                        <li>
+
+                            <input type="text" class="full text-center form-control" placeholder="Email ">
+                            <button class="btn btn-success btn-block" type="button"> Lorem ipsum <i
+                                        class="fa fa-long-arrow-right"> </i></button>
+                        </li>
+                    </ul>
+
+                    <div class="row">
+                        <a href="#"><img
+                                    src="http://th-live-03.slatic.net/layout/build/components/74ace6f971be60ab5892661b2af3191c.svg"></a>
+                        <a href="#"><img
+                                    src="http://th-live-03.slatic.net/layout/build/components/fd3bc53e70ef2728639cb81b1b1f3801.svg"></a>
+                        <a href="#"><img
+                                    src="http://th-live-03.slatic.net/layout/build/components/16f48950dfd55bbf99a23f226a71c855.svg"></a>
+                        <a href="#"><img
+                                    src="http://th-live-03.slatic.net/layout/build/components/d7a8ea4c011448077ddafab5f7196fee.svg"></a>
+                        <a href="#"><img
+                                    src="http://th-live-03.slatic.net/layout/build/components/d1165e8d5c7c2a8b98b78d278fdd437f.svg"></a>
+                        <a href="#"><img
+                                    src="http://th-live-03.slatic.net/layout/build/components/2d068c182611d697a5ca56be906ef76b.svg"></a>
+                        <a href="#"><img
+                                    src="http://th-live-03.slatic.net/layout/build/components/aab8f84c33b570189151768a8d389f0b.svg"></a>
+                        <a href="#"><img
+                                    src="http://th-live-03.slatic.net/layout/build/components/1611b4567278576343a376198f06127d.svg"></a>
+                    </div>
+
+                </div>
+            </div>
+            <!--/.row-->
+        </div>
+        <!--/.container-->
+    </div>
+
+    <!--/.footer-->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="col-md-6">
+                <h5>Payment method</h5>
+                <img src="https://cdn-p.weloveshopping.com/themes/titan/assets/images/footer/chpay_logo.png"><br> <span
+                        class="ft-copy-right">Copyright © 2017 Food Delivery Commerce Co., Ltd. All Rights Reserved.</span>
+            </div>
+            <div class="col-md-3 col-md-offset-3">
+                <div class="ft-title"><h5>Verified by</h5></div>
+                <img src="https://cdn-p.weloveshopping.com/themes/titan/assets/images/footer/ssl_dbd_logo.png">
+            </div>
+        </div>
+    </div>
+    <!--/.footer-bottom-->
+</footer>
 </body>
 </html>
 
