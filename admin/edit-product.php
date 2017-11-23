c58f9af356279e3e57c473b29a3ec884a7fca0fc
Pun <pannathornn@gmail.com>
2017-11-23 12:28:02 +0700

    Add Java script

--- admin/edit-product.php
+++ admin/edit-product.php
@@ -88,3 +88,33 @@ if (mysqli_num_rows($result) > 0)
 
 </body>
 </html>
+<script>
+    $(document).ready(function () {
+        ingreFields();
+    });
+    $("#edit-product").on('submit', (function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "php-action/edit-productSQL.php", // Url to which the request is send
+            type: "POST",             // Type of request to be send, called as method
+            data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+            contentType: false,       // The content type used when sending data to the server.
+            cache: false,             // To unable request pages to be cached
+            processData: false,        // To send DOMDocument or non processed data file it is set to false
+            success: function (data)   // A function to be called if request succeeds
+            {
+                swal('Updated!', data, 'success');
+            }
+        });
+    }));
+    function ingreFields() {
+        $(".add-more").click(function () {
+            var html = $(".copy-fields").html();
+            $(".after-add-more").after(html);
+        });
+        //here it will remove the current value of the remove button which has been pressed
+        $("body").on("click", ".remove", function () {
+            $(this).parents(".control-group").remove();
+        });
+    }
+</script>
