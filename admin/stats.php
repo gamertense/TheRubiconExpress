8e1e1f252eab0f2d822c9354648bbb2e0d6509cb
Antonio Jack <gamertense@live.com>
2017-12-15 23:46:09 +0700

    Add "Income" graph.

--- admin/stats.php
+++ admin/stats.php
@@ -19,9 +19,24 @@ require_once('../dbconfig.php'); ?>
 <script>
     var month = [];
     var quantity = [];
+    var price = [];
 </script>
 
-<?php $query = "select monthname(orderDate) as monthName, sum(quantity) as qty from orders group by monthname(orderDate) order by orderDate";
+<?php
+$query = "select monthname(orderDate) as monthName, sum(price*quantity) as price
+            from orders o join product p on p.product_id = o.product_id
+            group by monthname(orderDate) order by orderDate";
+//$query = "select monthname(orderDate) as monthName, sum(quantity) as qty from orders group by monthname(orderDate) order by orderDate";
+$result = mysqli_query($connect, $query);
+if (mysqli_num_rows($result) > 0):
+    while ($row = mysqli_fetch_array($result)): ?>
+        <script>
+            price.push("<?= $row['price'] ?>");
+        </script>
+    <?php endwhile;
+endif;
+
+$query = "select monthname(orderDate) as monthName, sum(quantity) as qty from orders group by monthname(orderDate) order by orderDate";
 $result = mysqli_query($connect, $query);
 if (mysqli_num_rows($result) > 0):
     while ($row = mysqli_fetch_array($result)): ?>
@@ -36,8 +51,15 @@ endif;
 ?>
 <br>
 <div class="container">
-    <div class="chart-container" style="position: relative; height:40vh; width:60vw">
-        <canvas id="myChart"></canvas>
+    <div class="row">
+        <div class="chart-container" style="position: relative; height:40vh; width:60vw">
+            <canvas id="myChart"></canvas>
+        </div>
+    </div>
+    <div class="row">
+        <div class="chart-container" style="position: relative; height:40vh; width:60vw; margin-top: 200px">
+            <canvas id="myChart2"></canvas>
+        </div>
     </div>
 </div>
 </body>
@@ -55,6 +77,30 @@ endif;
                 data: quantity,
                 backgroundColor: 'rgb(64, 108, 249)',
                 borderColor: 'rgb(64, 108, 249)',
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    var ctx2 = document.getElementById("myChart2").getContext('2d');
+    var myChart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: month,
+            datasets: [{
+                label: 'Income',
+                data: price,
+                backgroundColor: 'rgb(255, 0, 0)',
+                borderColor: 'rgb(255, 0, 0)',
                 fill: false
             }]
         },
