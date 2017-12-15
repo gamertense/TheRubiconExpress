00d0d2c1f370203fd4ef658f291d72eb2f703b25
Antonio Jack <gamertense@live.com>
2017-12-15 23:27:55 +0700

    Remove products that have no image.

--- rubicon.sql
+++ rubicon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2017 at 04:03 PM
+-- Generation Time: Dec 15, 2017 at 05:27 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -35,13 +35,6 @@ CREATE TABLE `cart` (
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`cart_id`, `cu_id`, `product_id`, `quantity`) VALUES
-(1, 1, 4, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -60,7 +53,7 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`category_id`, `category_name`, `category_product`) VALUES
 (1, 'Electronic devices', ''),
-(2, 'Food', '');
+(2, 'Games', '');
 
 -- --------------------------------------------------------
 
@@ -105,8 +98,8 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`cu_id`, `email`, `password`, `name`, `address`, `usertype`, `isapproved`) VALUES
 (1, 'a@a.com', '123', 'Antonio Jack', 'This is an address', '', 1),
-(2, 'b@b.com', '123', 'John Doe', 'John Doe\'s address', '', 1),
-(3, 'c@c.com', '123', 'Jane Doe', 'Jane Doe\'s address', '', 1),
+(2, 'b@b.com', '123', 'John Doe', 'John Doe\'s address', '', 0),
+(3, 'c@c.com', '123', 'Jane Doe', 'Jane Doe\'s address', '', 0),
 (6, 'a@a.com', '123', 'Chayaphat', '', 'User', 1),
 (7, 'a@a.com', '123', 'dsdf', '', 'User', 1);
 
@@ -130,36 +123,34 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`order_id`, `cu_id`, `product_id`, `orderDate`, `quantity`, `isDelivered`) VALUES
-(1, 3, 2, '2017-01-22', 1, 1),
-(2, 1, 2, '2017-01-22', 2, 1),
-(3, 2, 2, '2017-02-22', 3, 1),
-(4, 2, 2, '2017-03-22', 3, 1),
-(5, 2, 2, '2017-03-22', 2, 0),
-(6, 1, 2, '2017-04-22', 2, 1),
-(7, 1, 2, '2017-04-22', 3, 0),
-(8, 2, 2, '2017-04-22', 2, 0),
-(9, 3, 2, '2017-04-22', 2, 0),
-(10, 3, 2, '2017-04-22', 3, 0),
-(11, 3, 2, '2017-05-22', 2, 0),
-(12, 1, 2, '2017-05-22', 1, 0),
-(13, 1, 2, '2017-05-22', 1, 0),
-(14, 3, 2, '2017-06-22', 1, 0),
-(15, 2, 2, '2017-07-22', 2, 0),
-(16, 2, 2, '2017-07-22', 3, 0),
-(17, 1, 2, '2017-07-22', 1, 0),
-(18, 3, 2, '2017-08-22', 3, 0),
-(19, 3, 2, '2017-08-22', 3, 0),
-(20, 1, 2, '2017-08-22', 3, 0),
-(21, 3, 2, '2017-09-22', 2, 0),
-(22, 1, 2, '2017-09-22', 1, 0),
-(23, 2, 2, '2017-09-22', 1, 0),
-(24, 2, 2, '2017-09-22', 2, 0),
-(25, 2, 2, '2017-09-22', 2, 0),
-(26, 1, 2, '2017-10-22', 1, 0),
-(27, 3, 2, '2017-10-22', 3, 0),
-(28, 3, 2, '2017-10-22', 2, 0),
-(29, 1, 2, '2017-10-22', 1, 0),
-(30, 2, 2, '2017-10-22', 1, 0);
+(1, 1, 10, '2017-01-22', 1, 0),
+(2, 3, 7, '2017-02-22', 2, 0),
+(3, 3, 9, '2017-02-22', 2, 0),
+(4, 3, 9, '2017-03-22', 1, 0),
+(5, 3, 3, '2017-03-22', 3, 0),
+(6, 3, 10, '2017-03-22', 1, 0),
+(7, 3, 13, '2017-04-22', 2, 0),
+(8, 3, 10, '2017-04-22', 3, 0),
+(9, 3, 8, '2017-04-22', 2, 0),
+(10, 3, 13, '2017-04-22', 2, 0),
+(11, 3, 10, '2017-04-22', 1, 0),
+(12, 1, 7, '2017-05-22', 1, 0),
+(13, 1, 8, '2017-05-22', 1, 0),
+(14, 2, 8, '2017-05-22', 2, 0),
+(15, 2, 2, '2017-06-22', 3, 0),
+(16, 2, 7, '2017-06-22', 1, 0),
+(17, 2, 12, '2017-06-22', 2, 0),
+(18, 2, 4, '2017-06-22', 3, 0),
+(19, 1, 12, '2017-08-22', 3, 0),
+(20, 3, 3, '2017-08-22', 2, 0),
+(21, 3, 13, '2017-09-22', 3, 0),
+(22, 2, 12, '2017-09-22', 2, 0),
+(23, 1, 10, '2017-09-22', 2, 0),
+(24, 1, 3, '2017-09-22', 2, 0),
+(25, 3, 5, '2017-10-22', 2, 0),
+(26, 1, 10, '2017-10-22', 1, 0),
+(27, 2, 10, '2017-10-22', 2, 0),
+(28, 3, 5, '2017-10-22', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -192,10 +183,7 @@ INSERT INTO `product` (`product_id`, `name`, `price`, `image`, `specification`, 
 (10, 'Horizon Zerodawn', 1860.00, 'game-images/HZD.jpg', 'PS4|Singleplayer|Openworld', 0, 2),
 (11, 'Assassin\'s creed Origin', 1980.00, 'game-images/ACO.jpg', 'PC|PS4|XBOX1|Singleplayer|Openworld', 0, 2),
 (12, 'Zelda Breath of the Wild', 1670.00, 'game-images/Z-BOTW.jpg', 'Switch|Singleplayer|Openworld', 0, 2),
-(13, 'Civilization VI', 1540.00, 'game-images/CIV6.jpg', 'PC|Strategy', 0, 2),
-(15, 'Crispy Wonton', 30.00, 'food-images/crispy-pork-wontons.jpg', '', 0, 2),
-(17, 'Honey Rost Duck', 70.00, 'food-images/HoneyRoastedDuck.jpg', '', 0, 2),
-(18, 'Quick-Fried Water Spinach Seasoned With Chili And Soy Sauce', 45.00, 'food-images/maxresdefault (2).jpg', '', 0, 2);
+(13, 'Civilization VI', 1540.00, 'game-images/CIV6.jpg', 'PC|Strategy', 0, 2);
 
 -- --------------------------------------------------------
 
@@ -270,7 +258,7 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -294,13 +282,13 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
