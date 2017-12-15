5efeace679dcc44781d181d4963c97d7df884e70
Antonio Jack <gamertense@live.com>
2017-12-16 01:46:11 +0700

    Update DB

--- rubicon.sql
+++ rubicon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2017 at 05:27 PM
+-- Generation Time: Dec 15, 2017 at 07:45 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -65,16 +65,16 @@ CREATE TABLE `comment` (
   `com_id` int(11) NOT NULL,
   `com_name` varchar(50) NOT NULL,
   `com_comment` text NOT NULL,
-  `food_id` int(11) NOT NULL
+  `product_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `comment`
 --
 
-INSERT INTO `comment` (`com_id`, `com_name`, `com_comment`, `food_id`) VALUES
-(22, 'pon', 'test', 2),
-(27, 'Antonio Jack', 'à¸°à¸³à¸«à¸°à¸°à¸°', 2);
+INSERT INTO `comment` (`com_id`, `com_name`, `com_comment`, `product_id`) VALUES
+(1, 'Antonio Jack', 'Good design', 2),
+(2, 'Pon', 'What a nice-looking phone!', 2);
 
 -- --------------------------------------------------------
 
@@ -98,10 +98,8 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`cu_id`, `email`, `password`, `name`, `address`, `usertype`, `isapproved`) VALUES
 (1, 'a@a.com', '123', 'Antonio Jack', 'This is an address', '', 1),
-(2, 'b@b.com', '123', 'John Doe', 'John Doe\'s address', '', 0),
-(3, 'c@c.com', '123', 'Jane Doe', 'Jane Doe\'s address', '', 0),
-(6, 'a@a.com', '123', 'Chayaphat', '', 'User', 1),
-(7, 'a@a.com', '123', 'dsdf', '', 'User', 1);
+(2, 'p@p.com', '123', 'Pon', 'John Doe\'s address', '', 1),
+(3, 'c@c.com', '123', 'Jane Doe', 'Jane Doe\'s address', '', 0);
 
 -- --------------------------------------------------------
 
@@ -150,7 +148,35 @@ INSERT INTO `orders` (`order_id`, `cu_id`, `product_id`, `orderDate`, `quantity`
 (25, 3, 5, '2017-10-22', 2, 0),
 (26, 1, 10, '2017-10-22', 1, 0),
 (27, 2, 10, '2017-10-22', 2, 0),
-(28, 3, 5, '2017-10-22', 2, 0);
+(28, 3, 5, '2017-10-22', 2, 0),
+(29, 2, 13, '2017-01-22', 1, 0),
+(30, 3, 11, '2017-02-22', 2, 0),
+(31, 3, 11, '2017-02-22', 3, 0),
+(32, 1, 9, '2017-03-22', 2, 0),
+(33, 3, 7, '2017-03-22', 2, 0),
+(34, 3, 5, '2017-03-22', 1, 0),
+(35, 1, 2, '2017-03-22', 3, 0),
+(36, 1, 5, '2017-04-22', 1, 0),
+(37, 3, 10, '2017-04-22', 1, 0),
+(38, 1, 9, '2017-05-22', 2, 0),
+(39, 1, 13, '2017-05-22', 3, 0),
+(40, 1, 3, '2017-05-22', 3, 0),
+(41, 3, 13, '2017-05-22', 3, 0),
+(42, 3, 12, '2017-06-22', 3, 0),
+(43, 1, 7, '2017-06-22', 2, 0),
+(44, 1, 5, '2017-06-22', 1, 0),
+(45, 2, 9, '2017-06-22', 2, 0),
+(46, 1, 11, '2017-06-22', 1, 0),
+(47, 1, 10, '2017-07-22', 2, 0),
+(48, 1, 12, '2017-07-22', 3, 0),
+(49, 3, 2, '2017-07-22', 1, 0),
+(50, 1, 2, '2017-08-22', 1, 0),
+(51, 2, 4, '2017-09-22', 1, 0),
+(52, 1, 13, '2017-09-22', 1, 0),
+(53, 3, 2, '2017-09-22', 3, 0),
+(54, 3, 4, '2017-09-22', 3, 0),
+(55, 3, 8, '2017-10-22', 2, 0),
+(56, 2, 13, '2017-10-22', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -162,28 +188,30 @@ CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `price` double(10,2) NOT NULL,
+  `discount` tinyint(2) NOT NULL,
   `image` varchar(255) NOT NULL,
   `specification` text NOT NULL,
   `comment` int(11) NOT NULL,
-  `category_id` int(20) NOT NULL
+  `category_id` int(20) NOT NULL,
+  `in_stock` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`product_id`, `name`, `price`, `image`, `specification`, `comment`, `category_id`) VALUES
-(2, 'Samsung Galaxy S8 ', 22000.00, 'product-images/s8.jpg', '1440p AMOLED Display|8 Gbs of RAM|Android 6.0|Wireless charging|USB-c', 0, 1),
-(3, 'iPhone X', 40000.00, 'product-images/ix.jpg', '1440p AMOLED display|Face-scanner|Apple Logo', 0, 1),
-(4, 'Samsung Galaxy Note 8', 32000.00, 'product-images/note8.jpg', '', 0, 1),
-(5, 'google pixel 2', 33000.00, 'product-images/google.jpg', '', 0, 1),
-(7, 'HTC One A9', 15000.00, 'product-images/one.jpg', '', 0, 1),
-(8, 'Nokia 5', 9000.00, 'product-images/nikia.jpg', '', 0, 1),
-(9, 'The Last of Us', 1470.00, 'game-images/TLOU.jpg', 'PS4|Singleplayer|Multiplayer', 0, 2),
-(10, 'Horizon Zerodawn', 1860.00, 'game-images/HZD.jpg', 'PS4|Singleplayer|Openworld', 0, 2),
-(11, 'Assassin\'s creed Origin', 1980.00, 'game-images/ACO.jpg', 'PC|PS4|XBOX1|Singleplayer|Openworld', 0, 2),
-(12, 'Zelda Breath of the Wild', 1670.00, 'game-images/Z-BOTW.jpg', 'Switch|Singleplayer|Openworld', 0, 2),
-(13, 'Civilization VI', 1540.00, 'game-images/CIV6.jpg', 'PC|Strategy', 0, 2);
+INSERT INTO `product` (`product_id`, `name`, `price`, `discount`, `image`, `specification`, `comment`, `category_id`, `in_stock`) VALUES
+(2, 'Samsung Galaxy S8 ', 22000.00, 15, 'product-images/s8.jpg', '1440p AMOLED Display|8 Gbs of RAM|Android 6.0|Wireless charging|USB-c', 0, 1, 1),
+(3, 'iPhone X', 40000.00, 0, 'product-images/ix.jpg', '1440p AMOLED display|Face-scanner|Apple Logo', 0, 1, 0),
+(4, 'Samsung Galaxy Note 8', 32000.00, 0, 'product-images/note8.jpg', '', 0, 1, 1),
+(5, 'google pixel 2', 33000.00, 0, 'product-images/google.jpg', '', 0, 1, 1),
+(7, 'HTC One A9', 15000.00, 0, 'product-images/one.jpg', '', 0, 1, 1),
+(8, 'Nokia 5', 9000.00, 0, 'product-images/nikia.jpg', '', 0, 1, 1),
+(9, 'The Last of Us', 1470.00, 0, 'game-images/TLOU.jpg', 'PS4|Singleplayer|Multiplayer', 0, 2, 1),
+(10, 'Horizon Zerodawn', 1860.00, 0, 'game-images/HZD.jpg', 'PS4|Singleplayer|Openworld', 0, 2, 1),
+(11, 'Assassin\'s creed Origin', 1980.00, 0, 'game-images/ACO.jpg', 'PC|PS4|XBOX1|Singleplayer|Openworld', 0, 2, 1),
+(12, 'Zelda Breath of the Wild', 1670.00, 0, 'game-images/Z-BOTW.jpg', 'Switch|Singleplayer|Openworld', 0, 2, 1),
+(13, 'Civilization VI', 1540.00, 0, 'game-images/CIV6.jpg', 'PC|Strategy', 0, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -258,7 +286,7 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -270,19 +298,19 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `com_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `com_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `cu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `cu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `product`
